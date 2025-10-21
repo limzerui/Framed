6b2c2005be4f35aa3dfc50adb048a2ffea2c197f
limzerui <lim.zerui@gmail.com>
2025-10-21 09:49:49 +0800

    feat: add Microsoft Clarity tracking script to RootLayout for enhanced user insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,6 +77,18 @@ export default function RootLayout({
       className={`${inter.variable} ${geist.variable} ${geistMono.variable} ${sourceSerif4.variable} antialiased`}
     >
       <head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_PROJECT_ID}");
+`,
+          }}
+        />
         {/* Google Analytics */}
         {process.env.NEXT_PUBLIC_GA_ID && (
           <>
